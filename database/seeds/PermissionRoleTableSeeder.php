b5acc8337b66f4d447329967a828db412de98c50
Deivi Peña <systemson@gmail.com>
2019-01-25 22:47:22 -0400

    Updates

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -12,12 +12,23 @@ class PermissionRoleTableSeeder extends Seeder
      */
     public function run()
     {
-        $total = Permission::count();
-
         $permissions = [];
 
+        // Administrator permissions
+        $total = Permission::count();
         for ($x=1; $x<= $total; $x++) {
             $permissions[] = ['role_id' => 2, 'permission_id' => $x];
+        }
+
+        // Manager permissions
+        $manager_permissions = Permission::where('module', 'Articles')
+        ->orWhere('module', 'Categories')
+        ->orWhere('module', 'Menus')
+        ->orWhere('module', 'Components')
+        ->get(['id']);
+
+        foreach ($manager_permissions as $value) {
+            $permissions[] = ['role_id' => 3, 'permission_id' => $value->id];
         }
 
         DB::table('permission_role')->insert($permissions);
