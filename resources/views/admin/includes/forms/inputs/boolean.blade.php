ca9dea2ebfc4757ea028ec2dccd24eba042eace5
Deivi Peña <systemson@gmail.com>
2019-02-05 15:47:40 -0400

    Adding boolean input

--- resources/views/admin/includes/forms/inputs/boolean.blade.php
+++ resources/views/admin/includes/forms/inputs/boolean.blade.php
@@ -1,11 +1,47 @@
+@section('styles')
+    @parent
+
+  <style type="text/css">
+    .btn-outline-success:not(:disabled):not(.disabled).active,
+    .btn-outline-success:not(:disabled):not(.disabled):active {
+      color: #fff;
+      background-color: #28a745;
+      border-color: #28a745;
+    }
+    .btn-outline-danger:not(:disabled):not(.disabled).active,
+    .btn-outline-danger:not(:disabled):not(.disabled):active {
+      color: #fff;
+      background-color: #dc3545;
+      border-color: #dc3545;
+    }
+    .btn-outline-success:not(:disabled):not(.disabled):not(.active),
+    .btn-outline-success:not(:disabled):not(.disabled):not(.active) {
+      border-color: #28a745;
+    }
+    .btn-outline-danger:not(:disabled):not(.disabled):not(.active),
+    .btn-outline-danger:not(:disabled):not(.disabled):not(.active) {
+      border-color: #dc3545;
+    }
+  </style>
+@endsection
+
 <div class="form-group{{ $errors->has($input) ? ' has-error' : '' }}">
-  {{ Form::label($input, __($name . '.table.' . $input) . ' (*)', array('class' => 'col-sm-4 control-label')) }}
-  <div class="col-sm-8">
-    {{ Form::text($input, $value, array('class' => 'col-sm-12 control-form', 'placeholder' => __($name . '.table.' . $input))) }}
+  <div class="btn-group btn-group-toggle col-sm-12">
+    
+  <div class="col-sm-8 col-sm-offset-4" data-toggle="buttons">
+    <label class="btn btn-outline-success col-sm-6 {{ $value ? 'active' : null}}">
+      {{ Form::radio($input, 1, $value) }}
+      Yes
+    </label>
+    <label class="btn btn-outline-danger col-sm-6 {{ !$value ? 'active' : null}}">
+      {{ Form::radio($input, 0, !$value) }}
+      No
+    </label>
     @if ($errors->has($input))
     <span class="help-block">
       <strong>{{ $errors->first($input) }}</strong>
     </span>
     @endif
+  </div>
   </div>
 </div>
